9cfa95a86d95272881476dbb372733ecac06444a
Connor Meth <connormeth27@gmail.com>
2019-08-19 18:19:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  const arr = [];
+  let n = 0;
+  while (facts.length > n) {
+    arr.push(`${facts[n]}!!!`);
+  }
+  return array;
+}
+
+
+
+
+
